1c913d7dde38fac9a54236e7b4024e171cc7f1e4
ArturiaTW <ArturiaTW@gmail.com>
2023-08-15 23:40:18 +0800

    feat: 撰寫共用的元件（下拉式選單）

---
+++ src/ui/Dropdown.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import './Dropdown.css';
+
+function Dropdown({ options, onChange, defaultLabel }) {
+  const [isOptionVisible, setIsOptionVisible] = useState(null);
+  const [currentOption, setCurrentOption] = useState(null);
+
+  const toggleOptionVisible = () =>
+    setIsOptionVisible((prevVisible) => !prevVisible);
+
+  const onOptionClick = (option) => {
+    setCurrentOption(option);
+    setIsOptionVisible(false);
+  };
+
+  useEffect(() => {
+    onChange(currentOption?.value ?? null);
+  }, [currentOption, onChange]);
+
+  return (
+    <div className="select-container">
+      <div>
+        <div className="select" onClick={toggleOptionVisible}>
+          {!currentOption ? (
+            <span className="default-label">{`${defaultLabel}`}</span>
+          ) : (
+            <span className="label">{currentOption.label}</span>
+          )}
+          <div className={isOptionVisible ? 'arrow up' : 'arrow down'}></div>
+        </div>
+        <div className={!isOptionVisible ? `options` : `options visible`}>
+          {!Array.isArray(options)
+            ? null
+            : options.map((option) => (
+                <div
+                  className={
+                    currentOption?.value === option.value
+                      ? `option active`
+                      : `option`
+                  }
+                  onClick={() => onOptionClick(option)}
+                >
+                  {option.label}
+                </div>
+              ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
